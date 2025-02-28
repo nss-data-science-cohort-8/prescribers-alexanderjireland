badacf810e874e5c50dd4b26f4a40ba593d87098
alexanderjireland <irelandalexander1@gmail.com>
2025-02-27 18:04:44 -0600

    got through the first few problems

---
+++ prescriptions_query.sql
@@ -0,0 +1,102 @@
+-- 1. a. Which prescriber had the highest total number of claims (totaled over all drugs)? Report the npi and the total number of claims.
+SELECT npi, COUNT(npi) AS npi_frequency
+FROM prescription
+GROUP BY npi
+ORDER BY npi_frequency DESC;
+
+SELECT prescriber.npi, COUNT(prescription.npi) AS npi_frequency
+FROM prescriber
+FULL JOIN prescription ON prescriber.npi = prescription.npi
+GROUP BY prescriber.npi
+ORDER BY npi_frequency DESC;
+
+-- b. Repeat the above, but this time report the nppes_provider_first_name, nppes_provider_last_org_name, specialty_description, and the total number of claims.
+
+SELECT prescriber.nppes_provider_first_name, prescriber.nppes_provider_last_org_name, prescriber.specialty_description, npi_count.npi_frequency AS total_num_claims
+FROM(
+	SELECT prescriber.npi, COUNT(prescription.npi) AS npi_frequency
+	FROM prescriber
+	FULL JOIN prescription ON prescriber.npi = prescription.npi
+	GROUP BY prescriber.npi
+	ORDER BY npi_frequency DESC
+	) AS npi_count
+FULL JOIN prescriber ON npi_count.npi = prescriber.npi
+ORDER BY npi_frequency DESC;
+
+-- 2. a. Which specialty had the most total number of claims (totaled over all drugs)?
+
+SELECT specialty_description, SUM(total_num_claims) as specialty_total_num_claims
+FROM(
+	SELECT prescriber.nppes_provider_first_name, prescriber.nppes_provider_last_org_name, prescriber.specialty_description, npi_count.npi_frequency AS total_num_claims
+	FROM(
+		SELECT prescriber.npi, COUNT(prescription.npi) AS npi_frequency
+		FROM prescriber
+		FULL JOIN prescription ON prescriber.npi = prescription.npi
+		GROUP BY prescriber.npi
+		ORDER BY npi_frequency DESC
+		) AS npi_count
+	FULL JOIN prescriber ON npi_count.npi = prescriber.npi
+	ORDER BY npi_frequency DESC
+	) AS prescriber_total_claims
+GROUP BY specialty_description
+ORDER BY specialty_total_num_claims DESC;
+
+-- b. Which specialty had the most total number of claims for opioids?
+
+SELECT specialty_description, COUNT(specialty_description) AS specialty_total_opioid_claims
+FROM (
+	SELECT prescription.npi, prescription.drug_name, drug.opioid_drug_flag, prescriber.specialty_description
+	FROM prescriber
+	LEFT JOIN prescription ON prescriber.npi = prescription.npi
+	LEFT JOIN drug ON prescription.drug_name = drug.generic_name
+	WHERE opioid_drug_flag = 'Y'
+	) AS specialty_opioid_claims
+GROUP BY specialty_description
+ORDER BY specialty_total_opioid_claims DESC;
+
+-- c. Challenge Question: Are there any specialties that appear in the prescriber table that have no associated prescriptions in the prescription table?
+
+SELECT prescriber.npi, prescriber.specialty_description, prescriptions_specialties.specialty_description
+FROM prescriber
+LEFT JOIN (
+	SELECT prescription.npi, prescriber.specialty_description
+	FROM prescriber
+	INNER JOIN prescription ON prescriber.npi = prescription.npi
+	) AS prescriptions_specialties
+	ON prescriber.npi = prescriptions_specialties.npi
+WHERE prescriptions_specialties.npi IS NULL;
+
+-- d. Difficult Bonus: Do not attempt until you have solved all other problems! For each specialty, report the percentage of total claims by that specialty which are for opioids. Which specialties have a high percentage of opioids?
+
+-- 3. a. Which drug (generic_name) had the highest total drug cost?
+
+SELECT DISTINCT drug_name, total_drug_cost_ge65
+FROM prescription
+WHERE total_drug_cost_ge65 IS NOT NULL
+ORDER BY total_drug_cost_ge65 DESC;
+
+-- b. Which drug (generic_name) has the hightest total cost per day? Bonus: Round your cost per day column to 2 decimal places. Google ROUND to see how this works.
+
+-- 4. a. For each drug in the drug table, return the drug name and then a column named 'drug_type' which says 'opioid' for drugs which have opioid_drug_flag = 'Y', says 'antibiotic' for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs. Hint: You may want to use a CASE expression for this. See https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-case/
+
+-- b. Building off of the query you wrote for part a, determine whether more was spent (total_drug_cost) on opioids or on antibiotics. Hint: Format the total costs as MONEY for easier comparision.
+
+-- 5. a. How many CBSAs are in Tennessee? Warning: The cbsa table contains information for all states, not just Tennessee.
+
+-- b. Which cbsa has the largest combined population? Which has the smallest? Report the CBSA name and total population.
+
+-- c. What is the largest (in terms of population) county which is not included in a CBSA? Report the county name and population.
+
+-- 6. a. Find all rows in the prescription table where total_claims is at least 3000. Report the drug_name and the total_claim_count.
+
+-- b. For each instance that you found in part a, add a column that indicates whether the drug is an opioid.
+
+-- c. Add another column to you answer from the previous part which gives the prescriber first and last name associated with each row.
+
+-- 7. The goal of this exercise is to generate a full list of all pain management specialists in Nashville and the number of claims they had for each opioid. Hint: The results from all 3 parts will have 637 rows.
+
+-- a. First, create a list of all npi/drug_name combinations for pain management specialists (specialty_description = 'Pain Management) in the city of Nashville (nppes_provider_city = 'NASHVILLE'), where the drug is an opioid (opiod_drug_flag = 'Y'). Warning: Double-check your query before running it. You will only need to use the prescriber and drug tables since you don't need the claims numbers yet.
+
+-- b. Next, report the number of claims per drug per prescriber. Be sure to include all combinations, whether or not the prescriber had any claims. You should report the npi, the drug name, and the number of claims (total_claim_count).
+
+-- c. Finally, if you have not done so already, fill in any missing values for total_claim_count with 0. Hint - Google the COALESCE function.
